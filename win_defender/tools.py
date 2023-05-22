97f59cf6b47b7cc75fb2f2481c5dc7dda21e127a
Dhrumil Mistry <56185972+dmdhrumilmistry@users.noreply.github.com>
2023-05-22 15:42:16 +0530

    refactor project

--- win_defender/tools.py
+++ win_defender/tools.py
@@ -6,6 +6,32 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO,
                     format='[%(asctime)s] [%(levelname)s] - %(message)s')
 
+
+def sync_time(hostname:str='samay1.nic.in'):
+    '''Synchronize time with specified hostname time server on windows machine
+    
+    Args:
+        hostname (str): NTP hostname/IP. default: samay1.nic.1
+
+    Returns:
+        None
+    '''
+    _, rcode = run_cmd(
+        cmd='net start w32time',
+        succ_msg='w32time service started',
+        err_msg='Failed to start w32time service',
+        succ_rcode=0,
+    )
+
+    if rcode == 0:
+         run_cmd(
+            cmd=f'w32tm /config /update /manualpeerlist:{hostname}',
+            succ_msg=f'Time Synced with {hostname} successfully',
+            err_msg=f'Failed to sync time with {hostname}',
+            succ_rcode=0,
+        )
+       
+
 def block_root_hubs():
     '''Blocks USB root hubs on windows machine
     
@@ -30,6 +56,3 @@ def block_root_hubs():
                     succ_msg=f'USB {device_id} blocked',
                     err_msg=f'Cannot disable USB {device_id}'
                 )
-
-if __name__ == '__main__':
-    block_root_hubs()
